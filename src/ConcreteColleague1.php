2d228bb575b0be58fbfc6aa1823619d1520fd38f
Jagepard <dankorot@gmail.com>
2017-08-31 13:10:54 +0300

    Добавляет declare(strict_types = 1);

--- src/ConcreteColleague1.php
+++ src/ConcreteColleague1.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 /**
  * @author    : Korotkov Danila <dankorot@gmail.com>
  * @copyright Copyright (c) 2017, Korotkov Danila
@@ -12,13 +14,13 @@ namespace Behavioral\Mediator;
  * Class ConcreteColleague1
  * @package Behavioral\Mediator
  */
-class ConcreteColleague1
+class ConcreteColleague1 implements ColleagueInterface
 {
 
     /**
      * @param SomeEvent $event
      */
-    public function onEvent(SomeEvent $event)
+    public function onEvent(SomeEvent $event): void
     {
         $event->setMessage('How are you?');
         print $event->getMessage() . "\n";
