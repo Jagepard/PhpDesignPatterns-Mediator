18e206d2973a6c56c0918c4d982da750b6b348ff
Jagepard <dankorot@gmail.com>
2017-08-31 13:46:29 +0300

    Добавляет declare(strict_types = 1);

--- src/SomeEvent.php
+++ src/SomeEvent.php
@@ -1,17 +1,33 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Behavioral\Mediator;
 
+/**
+ * Class SomeEvent
+ *
+ * @package Behavioral\Mediator
+ */
 class SomeEvent
 {
+    /**
+     * @var
+     */
     protected $message;
 
-    public function setMessage($message)
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message)
     {
         $this->message = $message;
     }
 
-    public function getMessage()
+    /**
+     * @return string
+     */
+    public function getMessage(): string
     {
         return $this->message;
     }
