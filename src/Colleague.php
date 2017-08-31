616e99f8d12268e4011a90ff6c4f04d02bfd3d27
Jagepard <dankorot@gmail.com>
2017-08-31 13:07:49 +0300

    Добавляет файл

---
+++ src/Colleague.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2017, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace Behavioral\Mediator;
+
+/**
+ * Interface Colleague
+ *
+ * @package Behavioral\Mediator
+ */
+interface Colleague
+{
+
+}
