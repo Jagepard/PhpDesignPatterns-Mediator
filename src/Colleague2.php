2ae770875fe88d9316ac0dfb95ac35bcd03e9d71
Jagepard <dankorot@gmail.com>
2019-12-11 11:20:33 +0300

    renamed

--- src/Colleague2.php
+++ src/Colleague2.php
@@ -3,20 +3,20 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Behavioral\Mediator;
 
-class ConcreteColleague3 implements ListenerInterface
+class Colleague2 implements ListenerInterface
 {
     /**
      * @param HandlerInterface $handler
      */
     public function onEvent(HandlerInterface $handler): void
     {
-        $handler->setMessage('Colleague3: Fuck you all!');
+        $handler->setMessage('Colleague2: Fine, thanks!');
         printf("%s\n", $handler->getMessage());
     }
 }
