b5957ba010f109c9c71209565e071b6a79e18062
Jagepard <dankorot@gmail.com>
2020-04-16 12:22:08 +0300

    update MediatorInterface

--- src/MediatorInterface.php
+++ src/MediatorInterface.php
@@ -10,16 +10,15 @@ namespace Behavioral\Mediator;
 interface MediatorInterface
 {
     /**
-     * @param string           $name
      * @param AbstractListener $listener
      * @param string           $method
      */
-    public function addListener(string $name, AbstractListener $listener, string $method): void;
+    public function addListener(AbstractListener $listener, string $method): void;
 
     /**
-     * @param  string  $name
-     * @param  HandlerInterface|null  $handler
+     * @param string           $listenerName
+     * @param HandlerInterface $handler
      * @return mixed
      */
-    public function notify(string $name, HandlerInterface $handler = null);
+    public function notify(string $listenerName, HandlerInterface $handler);
 }
